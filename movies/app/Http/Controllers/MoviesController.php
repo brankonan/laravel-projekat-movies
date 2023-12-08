d86dc1a6b95fe6d1f93cd2a3c480d1b838adfd78
brankonan <branko.kovacevic09@gmail.com>
2023-12-08 10:12:34 -0800

    prva tri zadatka

---
+++ movies/app/Http/Controllers/MoviesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ movies/resources/views/movies/index.blade.php
@@ -0,0 +1,8 @@
+<h1>Lista Svih filmova</h1>
+
+<ul>
+    @foreach($movies as $movie)
+        <li>{{ $movie->title }} - {{ $movie->storyline }}</li>
+    @endforeach
+
+</ul>
